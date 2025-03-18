5f6f56f7ad37db167f1da13a815845a44b096879
SamuelJoseph <sammiejose.309@gmail.com>
2025-03-19 00:03:11 +0530

    Connected backend to local MongoDB with Mongoose and tested with mongosh

--- backend/index.js
+++ backend/index.js
@@ -1,4 +1,10 @@
 const express = require('express');
+const mongoose = require('mongoose');
 const app = express();
+
+mongoose.connect('mongodb://localhost:27017/smartEventManager')
+  .then(() => console.log('MongoDB connected'))
+  .catch(err => console.log('MongoDB connection error:', err));
+
 app.get('/', (req, res) => res.send('Backend running!'));
 app.listen(5000, () => console.log('Server on port 5000'));
